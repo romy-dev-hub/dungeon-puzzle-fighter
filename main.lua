22ade0d4a0ba0f08ffc5ee15b015a9649272fd0f
xiao <roumaissa.hadibi.dev@gmail.com>
2025-09-02 10:01:29 +0100

    added variables for now

--- main.lua
+++ main.lua
@@ -10,3 +10,16 @@ local currentState = 'exploration';
 
 
 --global variables
+local player = {
+    x = 100;
+    y = 100;
+    speed = 200;
+    hp = 100;
+    maxHp = 100;
+    level = 1;
+    xp = 0;
+}
+
+local enemy = nil;  -- will be set during battle
+local map = nil;
+local encounterChance = 0.05;  -- 5% per update cycle 
